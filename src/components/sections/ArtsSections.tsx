f04d20b7dea9b95abe7230c3f005200078b7c48f
Sam Mahonri <sammahonri06@gmail.com>
2024-06-10 22:46:20 -0300

    new: art selector and url sync

--- src/components/sections/ArtsSections.tsx
+++ src/components/sections/ArtsSections.tsx
@@ -1,15 +1,17 @@
 "use client";
 
 import { useState, useEffect, useRef } from "react";
+import { useSearchParams } from "next/navigation";
 import { ArtItem } from "@/lib/artistic";
 import Image from "next/image";
 import { Reveal } from "../fx/Motion";
 import placeholderImage from "/public/placeholders/img.jpeg";
 import clsx from "clsx";
 import { useTranslations } from "next-intl";
-import { XMarkIcon, HeartIcon } from "@heroicons/react/24/solid";
+import { XMarkIcon, HeartIcon, ShareIcon, CheckIcon } from "@heroicons/react/24/solid";
 import { Link } from "@/navigation";
 import Spinner from "../ui/Spinner";
+import { Share } from "next/font/google";
 
 interface Props {
     Arts: ArtItem[];
@@ -22,12 +24,21 @@ const Gallery = ({ Arts }: Props) => {
     const [hasMore, setHasMore] = useState(true);
     const [loadingMore, setLoadingMore] = useState(false);
     const loadMoreRef = useRef<HTMLDivElement>(null);
-
+    const searchParams = useSearchParams();
+    const [copied, setCopied] = useState(false);
     const tc = useTranslations("Common");
 
     useEffect(() => {
         setVisibleArts(Arts.slice(0, 9));
     }, [Arts]);
+
+    const handleCopyUrl = () => {
+        const url = window.location.href;
+        navigator.clipboard.writeText(url).then(() => {
+            setCopied(true);
+            setTimeout(() => setCopied(false), 2000); // Volta ao ícone original após 2 segundos
+        });
+    };
 
     useEffect(() => {
         const handleScroll = () => {
@@ -63,10 +74,22 @@ const Gallery = ({ Arts }: Props) => {
 
     const handleImageClick = (art: ArtItem) => {
         setFullScreenImage(art);
+        setLoadingStates(prev => ({
+            ...prev,
+            [-1]: true,
+        }));
+        const params = new URLSearchParams(window.location.search);
+        params.set("art", encodeURIComponent(art.imgUrl));
+        const newUrl = `${window.location.pathname}?${params.toString()}`;
+        window.history.pushState(null, '', newUrl);
     };
 
     const closeFullScreen = () => {
         setFullScreenImage(null);
+        const params = new URLSearchParams(window.location.search);
+        params.delete("art");
+        const newUrl = `${window.location.pathname}?${params.toString()}`;
+        window.history.pushState(null, '', newUrl);
     };
 
     const handleImageLoad = (index: number) => {
@@ -76,13 +99,31 @@ const Gallery = ({ Arts }: Props) => {
         }));
     };
 
+    useEffect(() => {
+        const artImgUrl = searchParams.get("art");
+        setTimeout(() => {
+            if (artImgUrl) {
+                const art = Arts.find(a => a.imgUrl === decodeURIComponent(artImgUrl));
+                if (art) {
+                    setFullScreenImage(art);
+                    setLoadingStates(prev => ({
+                        ...prev,
+                        [-1]: true,
+                    }));
+                }
+            }
+        }, 1000);
+        
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
     return (
         <div>
             <div className="md:grid md:grid-cols-3 flex flex-col items-center justify-center flex-grow w-full gap-3">
                 {visibleArts.map((art, index) => (
                     <div
                         key={index}
-                        className={clsx("border-2 border-secondary/20 hover:border-secondary w-full transition-all duration-300 cursor-pointer", {
+                        className={clsx("border-2 border-secondary/20 hover:border-secondary w-full transition-all duration-1000 cursor-pointer", {
                             "opacity-0": loadingStates[index] !== false,
                             "opacity-100": loadingStates[index] === false
                         })}
@@ -127,21 +168,19 @@ const Gallery = ({ Arts }: Props) => {
             {fullScreenImage && (
                 <div className="fixed top-0 z-50 left-0 w-full h-full flex flex-col justify-center items-center bg-background/90 pt-20" style={{zIndex: 9999}}>
                     <div className="relative w-full max-w-7xl flex-grow p-8 flex flex-col gap-2">
-                        <div className="flex flex-row items-center justify-center">
-                            <button className="btn btn-secondary" onClick={closeFullScreen}>
-                                <XMarkIcon /> {tc("close")} 
-                            </button>
-                            <Link href={fullScreenImage.postUrl} target="blank">
-                                <button className="btn btn-primary" onClick={closeFullScreen}>
-                                    <HeartIcon /> {tc("like")} 
-                                </button>
-                            </Link>
-                        </div>
+                        
+
+                        
+
                         <div style={{ 
                             position: "relative",
                             height: "100%",
                             width: "100%",
-                        }}>
+                        }} className={clsx(" transition-opacity duration-1000",{
+                            "opacity-0": loadingStates[-1] !== false,
+                            "opacity-100": loadingStates[-1] === false
+                        })}>
+                            
                             <Image
                                 alt=""
                                 placeholder="blur"
@@ -154,6 +193,22 @@ const Gallery = ({ Arts }: Props) => {
                                 sizes=""
                                 onLoad={() => handleImageLoad(-1)}
                             />
+                        </div>
+                        {loadingStates[-1] && <span className="flex w-full items-center justify-center py-8">
+                            <Spinner />
+                        </span>}
+                        <div className="flex flex-row items-center justify-center gap-1">
+                            <button className="btn btn-selector" onClick={closeFullScreen}>
+                                <XMarkIcon />
+                            </button>
+                            <Link href={fullScreenImage.postUrl} target="blank">
+                                <button className="btn btn-selector-secondary" onClick={closeFullScreen}>
+                                    <HeartIcon />
+                                </button>
+                            </Link>
+                            <button className="btn btn-selector-secondary" onClick={handleCopyUrl}>
+                                {copied ? <CheckIcon /> : <ShareIcon />}
+                            </button>
                         </div>
                     </div>
                 </div>
